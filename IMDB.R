f1e74956ef1ef57aa0243945a0f5665e8ef3394c
noahleavi11 <noahjlvtt@gmail.com>
2020-09-03 20:46:09 -0600

    first push... messing around

--- IMDB.R
+++ IMDB.R
@@ -19,11 +19,31 @@ summary(imdb)
 ## Scatterplot of Budget vs. Score
 ## Budget is in local currency, need to convert to common currency
 ggplot(data=imdb, mapping=aes(x=budget, y=imdb_score)) +
-  geom_point()
-imdb %>% filter(budget>100000000, country=="USA") %>% 
+  geom_
+limdb %>% filter(budget>100000000, country=="USA") %>% 
   select(movie_title)
 
 ## Scatterplot of gross vs imdb
 ggplot(data=imdb, mapping=aes(x=gross, y=imdb_score)) +
   geom_point()
 with(imdb, cor(gross, imdb_score, use="complete.obs"))
+
+
+a = unique(imdb['country'])
+
+
+countries_currency_units = read_csv("countries_currency_units.csv", col_names = c("Country","Currency", "Currency code"))
+country_codes = read_csv("country_codes.csv")
+exchange_rates = read_csv("exchange rates.csv", col_names = c("Currency code", "Currency name", "Units per USD", "USD per Unit"))
+
+newdf = countries_currency_units %>% inner_join(exchange_rates, by = "Currency code")
+
+adf = newdf %>% inner_join(country_codes, "Country")
+
+colnames(adf)[1] <- "country"
+
+fdf = left_join(x=imdb,y=adf,by = c("country" = "Alpha-3 code"),all.x=TRUE)
+
+#bdf = imdb %>% left_join(adf, by.x = "country", by.y = "Alpha-3 code")
+
+#cdf = bdf %>% left_join(adf, by.x = "country", by.y = "Alpha-3 code")
